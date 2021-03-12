2ff6e79f5fe0abab644131f8348668ee9f7cadcf
Hyeonju <hjulee12@gmail.com>
2021-03-12 13:36:07 +0900

    Modify: 게시물 업로드 기능 구현 중

--- posting/models.py
+++ posting/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Posts(models.Model):
+class Post(models.Model):
     user_id    = models.ForeignKey('user.Users', on_delete = models.DO_NOTHING)
     image_urls = models.CharField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
--- posting/views.py
+++ posting/views.py
@@ -3,16 +3,19 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
-from .models import Posts
+from .models     import Posts
+from user.models import Users
 
-#@login_decorator
 class PostingView(View):
+# @login_decorator
     def post(self, request):
         data = json.loads(request.body)
 
         try:
+            user = Users.objects.get(id=user.id)
+            print(user)
             Posts.objects.create(
-                user_id     = request.user.id,  # request.user will give you a User object representing the currently logged-in user.
+                user_id     = user.id,  # request.user will give you a User object representing the currently logged-in user.
                 image_urls  = data['image_urls'],
             )
 
@@ -21,9 +24,9 @@ class PostingView(View):
         except KeyError as e:
             return JsonResponse({'MESSAGE':'KEY_ERROR =>' + e.args[0]}, status=400)
 
-class PostingListView(View):
-    def get(self, request):
-        
-        try:
+#class PostingListView(View):
+#    def get(self, request):
+#        
+#        try:
 
 
